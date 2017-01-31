cb6ee0f3d0043c9510e3433e29e8c93325dedadb
l34p <hmg0228@gmail.com>
2017-01-31 12:19:43 +0900

    Add more comments

--- 01.Kernel32/Source/Page.c
+++ 01.Kernel32/Source/Page.c
@@ -5,6 +5,7 @@
  * Default PAGE SIZE is 2MB. (CR3 -> PML4TE -> PDPTE -> PDE)
  * Support up to 64GB. (Use 64 Page Directories)
  * Therefore, we use only one PML4T and one PDPT.
+ * Also, we use 1:1 mapping between linear and physical address for simplicity.
  *
  * PML4T -> 0x100000 ~ 0x101000, 4KB
  * PDPT  -> 0x101000 ~ 0x102000, 4KB
@@ -18,16 +19,19 @@ void kInitializePageTables(void)
   DWORD dwMappingAddress = 0;
   int i;
 
+  /* Initialize PML4T. */
   kSetPageEntryData(&(pstPML4TEntry[0]), 0x00, 0x101000, PAGE_FLAGS_DEFAULT, 0);
   for(i=1; i<PAGE_MAXENTRYCOUNT; ++i)
     kSetPageEntryData(&(pstPML4TEntry[i]), 0, 0, 0, 0);
 
+  /* Initialize PDPT. */
   for(i=0; i<64; ++i)
     kSetPageEntryData(&(pstPDPTEntry[i]), 0, 0x102000 + (i * PAGE_TABLESIZE), PAGE_FLAGS_DEFAULT, 0);
   
   for(i=64; i<PAGE_MAXENTRYCOUNT; ++i)
     kSetPageEntryData(&(pstPDPTEntry[i]), 0, 0, 0, 0);
 
+  /* Initialize PD. */
   for(i=0; i<PAGE_MAXENTRYCOUNT*64; ++i)
   {
     kSetPageEntryData(&(pstPDEntry[i]), (i * (PAGE_DEFAULTSIZE >> 20)) >> 12, dwMappingAddress, 
